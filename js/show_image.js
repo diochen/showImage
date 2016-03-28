2b062c390d80b26907bf03bb532c02453cce85ac
Dio <jhuhuchen@fareastone.com>
2016-03-28 17:06:05 +0800

    resize and orientation support

--- js/show_image.js
+++ js/show_image.js
@@ -32,9 +32,10 @@ var PicUrls = [
     "images/11.png"
 ];
 
+var manager = new Manager();
+
 window.onload = function() {
     var picIdx = 0;
-    var manager = new Manager();
     manager.init("id_daddy");
     var trigger = setInterval(function() {
         try {
@@ -50,12 +51,20 @@ window.onload = function() {
     }, 1000);
 };
 
+var viewResize = function(){
+    manager.handleResize();
+};
+
+window.addEventListener("resize", viewResize);
+
 function Manager () {
     this.picAry = [];
     this.containerWidth = 0;
     this.containerHeight = 0;
+    this.containerId = "";
 
     this.init = function(containerId){
+        this.containerId = containerId;
         //var randomIDs = this.shuffle(PicIds);
         var randomUrls =  this.shuffle(PicUrls);
 
@@ -84,31 +93,36 @@ function Manager () {
         }
     }
 
+    this.handleResize = function(){
+        if(this.containerId == "" || this.containerWidth==0 || this.containerHeight==0)
+            return;
+
+        var containerDiv = document.getElementById(this.containerId);
+        var changeRate = containerDiv.offsetWidth/this.containerWidth;
+        this.containerWidth = containerDiv.offsetWidth;
+        this.containerHeight = containerDiv.offsetHeight;
+
+
+        for(var i=0; i<this.picAry.length ; i++){
+            this.picAry[i].changePosition(changeRate);
+        }
+    }
+
     this.shuffle = function(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
-
-        // While there remain elements to shuffle...
         while (0 !== currentIndex) {
-
-            // Pick a remaining element...
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex -= 1;
-
-            // And swap it with the current element.
             temporaryValue = array[currentIndex];
             array[currentIndex] = array[randomIndex];
             array[randomIndex] = temporaryValue;
         }
-
         return array;
     }
 
     this.getRandomX = function() {
         var min = 0;
         var max = this.containerWidth > 0 ? Math.floor(this.containerWidth*0.75) : 0;
-        console.log(max);
-        console.log(Math.floor(Math.random() * (max - min + 1)) + min);
-        console.log("----");
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
@@ -124,18 +138,33 @@ function Manager () {
 function Picture (id, imgURL  ){
     this.div_id = id;
     this.imgURL = imgURL;
+    this.isHidden = true;
+    this.posX = 0;
+    this.posY = 0;
     var picDiv = document.getElementById(this.div_id);
     var picBlock = picDiv.getElementsByTagName('img')[0];
     picBlock.src = this.imgURL;
 }
 
 Picture.prototype.show = function(posX, posY){
+    this.posX = posX;
+    this.posY = posY;
     var picDiv = document.getElementById(this.div_id);
     picDiv.style.visibility = 'visible';
     picDiv.style.position = 'absolute';
     picDiv.style.left = posX + "px";
     picDiv.style.top = posY + "px";
     picDiv.style.backgroundColor = '#' + Math.random().toString(16).substring(2, 8);
+    this.isHidden = false;
 }
 
+Picture.prototype.changePosition = function(changeRate){
+    if(this.isHidden == true)
+        return;
+    var picDiv = document.getElementById(this.div_id);
+    this.posX = Math.floor(this.posX*changeRate);
+    this.posY = Math.floor(this.posY*changeRate);
+    picDiv.style.left = this.posX + "px";
+    picDiv.style.top = this.posY + "px";
+}
 
